a83438e596d2398bf5b3a9baf8b2f06eb2f03c44
sem1on <sgorlov21@gmail.com>
2022-03-23 17:59:50 +0300

    Добавлен функционал приложения и доработан дизайн

--- src/App.js
+++ src/App.js
@@ -1,63 +1,117 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-
-
-
-
-const WeatherIcons = {
-    "01d": "/react-weather-app/icons/sunny.svg",
-    "01n": "/react-weather-app/icons/night.svg",
-    "02d": "/react-weather-app/icons/day.svg",
-    "02n": "/react-weather-app/icons/cloudy-night.svg",
-    "03d": "/react-weather-app/icons/cloudy.svg",
-    "03n": "/react-weather-app/icons/cloudy.svg",
-    "04d": "/react-weather-app/icons/perfect-day.svg",
-    "04n": "/react-weather-app/icons/cloudy-night.svg",
-    "09d": "/react-weather-app/icons/rain.svg",
-    "09n": "/react-weather-app/icons/rain-night.svg",
-    "10d": "/react-weather-app/icons/rain.svg",
-    "10n": "/react-weather-app/icons/rain-night.svg",
-    "11d": "/react-weather-app/icons/storm.svg",
-    "11n": "/react-weather-app/icons/storm.svg",
-};
+import cloudyNight from './icons/cloudy-night.svg';
+import cloudy from './icons/cloudy.svg';
+import day from './icons/day.svg';
+import night from './icons/night.svg';
+import perfectDay from './icons/perfect-day.svg';
+import rainNight from './icons/rain-night.svg';
+import rain from './icons/rain.svg';
+import storm from './icons/storm.svg';
+import sunny from './icons/sunny.svg';
 
 
 function App() {
-  return (
-    <div className="app">
-        <div className="container">
-            <div className="search-panel">
-                <input className="search-input" type="text" placeholder="Enter city . . ."/>   
-                <button className="search-button">Go</button>
-            </div>
-            <div className="main-info">
-                <h1>Weather in City</h1>
-                <div className="curent-date"> Monday, 23:30</div>
-                <div className="info-weather">
-                    <div className="waether-icon">
-                        <img src={WeatherIcons[0]} alt="vvv" />
+
+    const [weather, setWeather] = useState({});
+    const [city, setCity] = useState('New York');
+
+    const apiKey = 'd299b9e2dd386315b396cca93c6c0a36'
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const searchCity = async () => {
+         
+        const response = await fetch(url);
+
+        if(!response.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+        }
+        const data = await response.json();
+        
+        setWeather({
+            name: data.name,
+            icon: data.weather[0].icon,
+            descr: data.weather[0].description,
+            temp: data.main.temp - 273,
+            humidity: data.main.humidity,
+            wind: data.wind.speed,
+            real: data.main.feels_like -273,
+        });
+        setCity('');
+    }
+
+    useEffect(() => {
+        searchCity()
+    }, []);
+
+    const weatherIcons = {
+        "01d": sunny,
+        "01n": night,
+        "02d": day,
+        "02n": cloudyNight,
+        "03d": cloudy,
+        "03n": cloudy,
+        "04d": perfectDay,
+        "04n": cloudyNight,
+        "09d": rain,
+        "09n": rainNight,
+        "10d": rain,
+        "10n": rainNight,
+        "11d": storm,
+        "11n": storm,
+    };
+
+    return (
+        <div className="app">
+            <div className="container">
+                <div className="search-panel">
+                    <input 
+                        className="search-input" 
+                        type="text" 
+                        placeholder="Enter city . . ."
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                        />   
+                    <button 
+                        className="search-button"
+                        type="submit"
+                        onClick={searchCity}
+                        >
+                            search
+                    </button>
+                </div>
+                <div className="main-info">
+                    <h1>{weather.name}</h1>
+                    <div className="curent-date"> 
+                        <p>{weather.descr}</p>
                     </div>
-                    <div className="info-temp">17 C</div>
-                    <div className="info-descr">
-                        <div className="info-item">
-                            <p>Humidity</p>
-                            <p>63%</p>
+                    <div className="info-weather">
+                        <div className="weather-icon">
+                            <img src={weatherIcons[weather.icon]} alt="icon" />
+                            
                         </div>
-                        <div className="info-item">
-                            <p>wind</p>
-                            <p>3.6 km/h</p>
-                        </div>
-                        <div className="info-item">
-                            <p>real feel</p>
-                            <p>19 c</p>
+                        <div className="info-temp">{Math.floor(weather.temp)}° C</div>
+                        <div className="info-descr">
+                            <div className="info-item">
+                                <p>Humidity</p>
+                                <p>{weather.humidity}%</p>
+                            </div>
+                            <div className="info-item">
+                                <p>wind</p>
+                                <p>{weather.wind} km/h</p>
+                            </div>
+                            <div className="info-item">
+                                <p>real feel</p>
+                                <p>{Math.floor(weather.real)}° C</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
